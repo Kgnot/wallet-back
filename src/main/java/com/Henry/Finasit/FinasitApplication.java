56cf06ad14a97495662941d96f7ceb0b04a3327e
Henry Ricaurte Mora <hricaurtem@proton.me>
2025-01-09 15:13:32 -0500

    Cambios obligatorios

--- src/main/java/com/Henry/Finasit/FinasitApplication.java
+++ src/main/java/com/Henry/Finasit/FinasitApplication.java
@@ -1,6 +1,5 @@
 package com.Henry.Finasit;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,13 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class FinasitApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .filename(".env")
-                .load();
-
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
-
-
-        SpringApplication.run(FinasitApplication.class, args);
+         SpringApplication.run(FinasitApplication.class, args);
     }
 }
